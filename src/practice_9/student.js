c8af8dbb4a5cac902758788bec18fcdb42a53134
Tian Ge <tge@CNtge.local>
2018-07-18 00:02:28 +0800

    practise_9

--- src/practice_9/student.js
+++ src/practice_9/student.js
@@ -1,1 +1,4 @@
+'use strict'
+import Student from "../practice_8/student"
 
+export default Student;
--- src/practice_9/teacher.js
+++ src/practice_9/teacher.js
@@ -1,1 +1,4 @@
+'use strict'
+import Teacher from "../practice_8/teacher"
 
+export default Teacher;
