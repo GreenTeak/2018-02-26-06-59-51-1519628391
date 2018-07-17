c169616aadc8b28a062231e383af447f05d00efd
Tian Ge <tge@CNtge.local>
2018-07-17 22:10:51 +0800

    practide_8

--- src/practice_7/student.js
+++ src/practice_7/student.js
@@ -1,1 +1,14 @@
-
+"use strict"
+import Person from "../practice_1/person"
+import Class from "../practice_7/class"
+class Student extends Person{
+    constructor(name, age, classNumber){
+        super(name, age);
+        this.klass = classNumber;
+    }
+    introduce(){
+        return super.introduce() +` I am a Student. I am at Class ${this.klass.number}.`;
+    }
+      
+}
+export default Student;
--- src/practice_7/teacher-option2.js
+++ src/practice_7/teacher-option2.js
@@ -1,1 +1,23 @@
+'use strict'
+import Person from "../practice_1/person"
+import Student from "../practice_4/student"
+class Teacher extends Person{
+    constructor(name, age, klass){
+       super(name, age);
+       this.klass = klass;
+    }
+    introduce(){
+        if(this.klass === undefined) 
+           return super.introduce() + `I am a Teacher. I teach No Class.`
+        return super.introduce() + ` I am a Teacher. I teach Class ${this.klass.number}.`
+    }
+
+    introduceWith(stu){
+        if(stu.klass.number === this.klass.number)
+           return super.introduce()+ ` I am a Teacher. I teach ${stu.name}.`;
+        return super.introduce() + ` I am a Teacher. I don't teach ${stu.name}.`;
+
+    }
+}
+export default Teacher;
 
